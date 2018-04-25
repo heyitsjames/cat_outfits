fa13b35f2362be5c4c7049da2c147cae0b69f3d3
James Rasmussen <james@podium.co>
2018-04-25 15:43:28 -0600

    add port process.env for heroku

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(9001, function () {
+var server = app.listen(port, function () {
     console.log("app running on port.", server.address().port);
 });
